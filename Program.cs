de6ba209d32a50a8b3f0fbd0dc4542c9b4f2ea92
sleevezipper <plakbal@gmail.com>
2020-12-27 14:54:18 +0100

    working notificationstate service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using hass_desktop_service.StateDetectors.Windows.Fullscreen;
 
 namespace hass_desktop_service
 {
@@ -11,13 +13,23 @@ namespace hass_desktop_service
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            else
+            {
+                // we only support MS Windows for now
+                throw new NotImplementedException("Your platform is not yet supported");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
+                    services.AddSingleton<UserNotificationStateDetector>();
                     services.AddHostedService<Worker>();
                 });
     }
--- Worker.cs
+++ Worker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using hass_desktop_service.StateDetectors.Windows.Fullscreen;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,10 +12,12 @@ namespace hass_desktop_service
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly UserNotificationStateDetector _userNotificationStateDetector;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, UserNotificationStateDetector userNotificationStateDetector)
         {
             _logger = logger;
+            this._userNotificationStateDetector = userNotificationStateDetector;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,6 +25,8 @@ namespace hass_desktop_service
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation($"Notificationstate: {this._userNotificationStateDetector.GetState()}");
+
                 await Task.Delay(1000, stoppingToken);
             }
         }
