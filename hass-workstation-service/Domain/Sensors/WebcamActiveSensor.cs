2a4a8f2e306c2870b398ef79fede6d9b6a06e612
sleevezipper <plakbal@gmail.com>
2021-01-03 19:16:13 +0100

    try using registry for webcam use

--- hass-workstation-service/Domain/Sensors/WebcamActiveSensor.cs
+++ hass-workstation-service/Domain/Sensors/WebcamActiveSensor.cs
@@ -1,7 +1,9 @@
 ﻿using hass_workstation_service.Communication;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Management;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
@@ -17,19 +19,7 @@ namespace hass_workstation_service.Domain.Sensors
         }
         public override string GetState()
         {
-            try
-            {
-                List<Process> procs = WhoIsLocking("\\Device\\00000041");
-                if (procs.Count > 0)
-                {
-                    return "True";
-                }
-                return "False";
-            }
-            catch (Exception)
-            {
-                return "Error";
-            }
+            return IsWebCamInUse() ? "True" : "False";
         }
         public override AutoDiscoveryConfigModel GetAutoDiscoveryConfig()
         {
@@ -43,140 +33,27 @@ namespace hass_workstation_service.Domain.Sensors
             });
         }
 
-        [StructLayout(LayoutKind.Sequential)]
-        struct RM_UNIQUE_PROCESS
+        private bool IsWebCamInUse()
         {
-            public int dwProcessId;
-            public System.Runtime.InteropServices.ComTypes.FILETIME ProcessStartTime;
-        }
-
-        const int RmRebootReasonNone = 0;
-        const int CCH_RM_MAX_APP_NAME = 255;
-        const int CCH_RM_MAX_SVC_NAME = 63;
-
-        enum RM_APP_TYPE
-        {
-            RmUnknownApp = 0,
-            RmMainWindow = 1,
-            RmOtherWindow = 2,
-            RmService = 3,
-            RmExplorer = 4,
-            RmConsole = 5,
-            RmCritical = 1000
-        }
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-        struct RM_PROCESS_INFO
-        {
-            public RM_UNIQUE_PROCESS Process;
-
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_APP_NAME + 1)]
-            public string strAppName;
-
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_SVC_NAME + 1)]
-            public string strServiceShortName;
-
-            public RM_APP_TYPE ApplicationType;
-            public uint AppStatus;
-            public uint TSSessionId;
-            [MarshalAs(UnmanagedType.Bool)]
-            public bool bRestartable;
-        }
-
-        [DllImport("rstrtmgr.dll", CharSet = CharSet.Unicode)]
-        static extern int RmRegisterResources(uint pSessionHandle,
-                                              UInt32 nFiles,
-                                              string[] rgsFilenames,
-                                              UInt32 nApplications,
-                                              [In] RM_UNIQUE_PROCESS[] rgApplications,
-                                              UInt32 nServices,
-                                              string[] rgsServiceNames);
-
-        [DllImport("rstrtmgr.dll", CharSet = CharSet.Auto)]
-        static extern int RmStartSession(out uint pSessionHandle, int dwSessionFlags, string strSessionKey);
-
-        [DllImport("rstrtmgr.dll")]
-        static extern int RmEndSession(uint pSessionHandle);
-
-        [DllImport("rstrtmgr.dll")]
-        static extern int RmGetList(uint dwSessionHandle,
-                                    out uint pnProcInfoNeeded,
-                                    ref uint pnProcInfo,
-                                    [In, Out] RM_PROCESS_INFO[] rgAffectedApps,
-                                    ref uint lpdwRebootReasons);
-
-        /// <summary>
-        /// Find out what process(es) have a lock on the specified file.
-        /// </summary>
-        /// <param name="path">Path of the file.</param>
-        /// <returns>Processes locking the file</returns>
-        /// <remarks>See also:
-        /// http://msdn.microsoft.com/en-us/library/windows/desktop/aa373661(v=vs.85).aspx
-        /// http://wyupdate.googlecode.com/svn-history/r401/trunk/frmFilesInUse.cs (no copyright in code at time of viewing)
-        /// 
-        /// </remarks>
-        static public List<Process> WhoIsLocking(string path)
-        {
-            uint handle;
-            string key = Guid.NewGuid().ToString();
-            List<Process> processes = new List<Process>();
-
-            int res = RmStartSession(out handle, 0, key);
-            if (res != 0) throw new Exception("Could not begin restart session.  Unable to determine file locker.");
-
-            try
+            using (var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam\NonPackaged"))
             {
-                const int ERROR_MORE_DATA = 234;
-                uint pnProcInfoNeeded = 0,
-                     pnProcInfo = 0,
-                     lpdwRebootReasons = RmRebootReasonNone;
-
-                string[] resources = new string[] { path }; // Just checking on one resource.
-
-                res = RmRegisterResources(handle, (uint)resources.Length, resources, 0, null, 0, null);
-
-                if (res != 0) throw new Exception("Could not register resource.");
-
-                //Note: there's a race condition here -- the first call to RmGetList() returns
-                //      the total number of process. However, when we call RmGetList() again to get
-                //      the actual processes this number may have increased.
-                res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, null, ref lpdwRebootReasons);
-
-                if (res == ERROR_MORE_DATA)
+                foreach (var subKeyName in key.GetSubKeyNames())
                 {
-                    // Create an array to store the process results
-                    RM_PROCESS_INFO[] processInfo = new RM_PROCESS_INFO[pnProcInfoNeeded];
-                    pnProcInfo = pnProcInfoNeeded;
-
-                    // Get the list
-                    res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, processInfo, ref lpdwRebootReasons);
-                    if (res == 0)
+                    using (var subKey = key.OpenSubKey(subKeyName))
                     {
-                        processes = new List<Process>((int)pnProcInfo);
-
-                        // Enumerate all of the results and add them to the 
-                        // list to be returned
-                        for (int i = 0; i < pnProcInfo; i++)
+                        if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
                         {
-                            try
+                            var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
+                            if (endTime <= 0)
                             {
-                                processes.Add(Process.GetProcessById(processInfo[i].Process.dwProcessId));
+                                return true;
                             }
-                            // catch the error -- in case the process is no longer running
-                            catch (ArgumentException) { }
                         }
                     }
-                    else throw new Exception("Could not list processes locking resource.");
                 }
-                else if (res != 0) throw new Exception("Could not list processes locking resource. Failed to get size of result.");
-            }
-            finally
-            {
-                RmEndSession(handle);
             }
 
-            return processes;
+            return false;
         }
-
     }
 }
