4915d9963249389ee543789e9cdb33f500b50828
department_g33k <brannon.dan@gmail.com>
2021-03-15 16:06:06 -0700

    Create Command for Hibernate
    Created a command to issue "shutdown /h" (I prefer to Hibernate my system).
    
    I don't have the toolchain to test or add option to the UI.  If this isn't a direction you'd like to go, please disregard.

---
+++ hass-workstation-service/Domain/Commands/HibernateCommand.cs
@@ -0,0 +1,17 @@
+﻿using hass_workstation_service.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hass_workstation_service.Domain.Commands
+{
+    public class HibernateCommand : CustomCommand
+    {
+        public HibernateCommand(MqttPublisher publisher, string name = "Hibernate", Guid id = default(Guid)) : base(publisher, "shutdown /h", name ?? "Hibernate", id)
+        {
+            this.State = "OFF";
+        }
+    }
+}
