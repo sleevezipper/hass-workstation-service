93cdde4cde07f710a379d672295d6d67655f7c5b
Jordan Janzen <v-jjanzen@microsoft.com>
2021-12-15 15:00:04 -0800

    Make microphone and webcam sensors report all active processes

--- hass-workstation-service/Domain/Sensors/MicrophoneProcessSensor.cs
+++ hass-workstation-service/Domain/Sensors/MicrophoneProcessSensor.cs
@@ -4,11 +4,14 @@ using System;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
+using System.Collections.Generic;
 
 namespace hass_workstation_service.Domain.Sensors
 {
     public class MicrophoneProcessSensor : AbstractSensor
     {
+        private List<string> processes = new List<string>();
+
         public MicrophoneProcessSensor(MqttPublisher publisher, int? updateInterval = null, string name = "MicrophoneProcess", Guid id = default) : base(publisher, name ?? "MicrophoneProcess", updateInterval ?? 10, id)
         {
         }
@@ -38,92 +41,52 @@ namespace hass_workstation_service.Domain.Sensors
         }
 
         [SupportedOSPlatform("windows")]
+        private void CheckLastUsed(RegistryKey key)
+        {
+            foreach (var subKeyName in key.GetSubKeyNames())
+            {
+                // NonPackaged has multiple subkeys
+                if (subKeyName == "NonPackaged")
+                {
+                    using (var nonpackagedkey = key.OpenSubKey(subKeyName))
+                    {
+                        CheckLastUsed(nonpackagedkey);
+                    }
+                }
+                else
+                {
+                    using (var subKey = key.OpenSubKey(subKeyName))
+                    {
+                        if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
+                        {
+                            var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
+                            if (endTime <= 0)
+                            {
+                                this.processes.Add(subKeyName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [SupportedOSPlatform("windows")]
         private string IsMicrophoneInUseRegistry()
         {
             using (var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone"))
             {
-                foreach (var subKeyName in key.GetSubKeyNames())
-                {
-                    // NonPackaged has multiple subkeys
-                    if (subKeyName == "NonPackaged")
-                    {
-                        using (var nonpackagedkey = key.OpenSubKey(subKeyName))
-                        {
-                            foreach (var nonpackagedSubKeyName in nonpackagedkey.GetSubKeyNames())
-                            {
-                                using (var subKey = nonpackagedkey.OpenSubKey(nonpackagedSubKeyName))
-                                {
-                                    if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
-                                    {
-                                        var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
-                                        if (endTime <= 0)
-                                        {
-                                            return nonpackagedSubKeyName;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        using (var subKey = key.OpenSubKey(subKeyName))
-                        {
-                            if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
-                            {
-                                var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
-                                if (endTime <= 0)
-                                {
-                                    return subKeyName;
-                                }
-                            }
-                        }
-                    }
-                }
+               CheckLastUsed(key);
             }
 
             using (var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone"))
             {
-                foreach (var subKeyName in key.GetSubKeyNames())
-                {
-                    // NonPackaged has multiple subkeys
-                    if (subKeyName == "NonPackaged")
-                    {
-                        using (var nonpackagedkey = key.OpenSubKey(subKeyName))
-                        {
-                            foreach (var nonpackagedSubKeyName in nonpackagedkey.GetSubKeyNames())
-                            {
-                                using (var subKey = nonpackagedkey.OpenSubKey(nonpackagedSubKeyName))
-                                {
-                                    if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
-                                    {
-                                        var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
-                                        if (endTime <= 0)
-                                        {
-                                            return nonpackagedSubKeyName;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        using (var subKey = key.OpenSubKey(subKeyName))
-                        {
-                            if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
-                            {
-                                var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
-                                if (endTime <= 0)
-                                {
-                                    return subKeyName;
-                                }
-                            }
-                        }
-                    }
-                }
+                CheckLastUsed(key);
             }
 
+            if (this.processes.Count() > 0)
+            {
+                return String.Join(",", this.processes.ToArray());
+            }
             return "off";
         }
     }
